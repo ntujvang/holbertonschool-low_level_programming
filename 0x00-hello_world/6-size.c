e1f85cf076ed448c19a42117a53434bc358e9096
nujivang <ntuj.vang@holbertonschool.com>
2016-10-12 22:09:57 +0000

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,14 +8,16 @@ int main(void)
 {
 	char k;
 	int i;
+	long int p;
+	long long int o;
 	float l;
 
 	printf("The size of a char: %lu.\n", (unsigned long)sizeof(k));
 	printf("The size of an int: %lu.\n", (unsigned long)sizeof(i));
-	printf("The size of a long int: %lu.\n", (unsigned long)sizeof(i));
-	printf("The size of a long long int: %lu.\n", (unsigned long long)sizeof
-	       (i));
-	printf("The size of a foat: %lu.\n" (unsigned long)sizeof(l));
+	printf("The size of a long int: %lu.\n", (unsigned long)sizeof(p));
+	printf("The size of a long long int: %lu.\n", (unsigned long)
+	       sizeof(o));
+	printf("The size of a foat: %lu.\n", (unsigned long)sizeof(l));
 
 	return (0);
 }
