51fcf2a532ade7ee99bd9c9d0bf568a03564a0e2
nujivang <ntuj.vang@holbertonschool.com>
2016-10-13 23:04:42 +0000

    10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - primary work area
+ *
+ * Return: 0 to end
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	i = '0';
+	j = '0';
+	while (i <= '9')
+	{
+		while (j <= '9')
+		{
+			putchar(i);
+			putchar(j);
+			if (i < '9' || j < '9')
+			putchar(',');
+			putchar(' ');
+			j++;
+		}
+		j = '0';
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
