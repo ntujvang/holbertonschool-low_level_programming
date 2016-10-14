06c0e58a2abb6a23c42f1d61469ccbd9f60c955d
nujivang <ntuj.vang@holbertonschool.com>
2016-10-14 18:54:10 +0000

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ * main - primary working stuff
+ *
+ * Return: 1 ends
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n",
+	      58);
+	return (1);
+}
