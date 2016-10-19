edb6c2a27126eb47cc0181e4f2c8e7b45b55db12
ntujvang <ntuj.vang@holbertonschool.com>
2016-10-19 05:15:13 +0000

    102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - working area
+ *
+ * Return: 0 to end
+ */
+int main(void)
+{
+	unsigned long int  p, i, n, k;
+
+	n = 1;
+	i = 2;
+	printf("1, 2, ");
+	for (k = 0; k <= 50; k++)
+	{
+		p = i + n;
+		printf("%lu, ", p);
+		n = i;
+		i = p;
+	}
+	printf("\n");
+	return (0);
+}
