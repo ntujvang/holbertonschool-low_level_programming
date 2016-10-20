3f022d7f14699915576c636b8f260092669a133e
ntujvang <ntuj.vang@holbertonschool.com>
2016-10-20 21:31:49 +0000

    9

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,18 @@ int main(void)
 	while (i <= 100)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else if (i % 5 == 0)
-			printf("Buzz ");
+			printf("Buzz");
 		else
-			printf("%d ", i);
+			printf("%d", i);
+		if (i != 100)
+			printf(" ");
+		else
+			printf("\n");
 		i++;
 	}
-	if (i != 100)
-		printf("\n");
 	return (0);
 }
