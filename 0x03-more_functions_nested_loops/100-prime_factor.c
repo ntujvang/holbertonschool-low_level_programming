c900c8583b5446eccc17313d6ef5ae65e5e1bb32
ntujvang <ntuj.vang@holbertonschool.com>
2016-10-20 22:47:20 +0000

    100

--- 0x03-more_functions_nested_loops/100-prime_factor.c
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	long int i, sum;
+	unsigned long int i, sum;
 
 	i = 612852475143;
 	if (i % 2 == 0)
-		printf("%li\n", sum = i / 2);
+		printf("%lu\n", sum = i / 2);
 	else
-		printf("%li\n", sum = i / 3);
+		printf("%lu\n", sum = i / 3);
 	return (0);
 }
