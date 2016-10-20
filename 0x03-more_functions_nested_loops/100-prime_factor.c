cadb690e2686d9db260c8444dd056f3ac7a47c13
ntujvang <ntuj.vang@holbertonschool.com>
2016-10-20 22:46:16 +0000

    100

---
+++ 0x03-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - working area
+ *
+ * Return: 0 to end
+ */
+int main(void)
+{
+	long int i, sum;
+
+	i = 612852475143;
+	if (i % 2 == 0)
+		printf("%li\n", sum = i / 2);
+	else
+		printf("%li\n", sum = i / 3);
+	return (0);
+}
