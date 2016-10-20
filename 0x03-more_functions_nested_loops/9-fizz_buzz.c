054e72e00a332da799c5a3a5f51ea1898b463214
ntujvang <ntuj.vang@holbertonschool.com>
2016-10-20 19:58:10 +0000

    9

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - working area
+ *
+ * Return: 0 to end
+ */
+int main(void)
+{
+	int i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
