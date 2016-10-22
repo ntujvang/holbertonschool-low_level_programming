ee68217d7812f2fcbb8bc7ecdd5f6ae7531f50de
ntujvang <ntuj.vang@holbertonschool.com>
2016-10-22 02:24:57 +0000

    101

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -9,24 +9,25 @@
  */
 void print_number(int n)
 {
-	long max[10];
-	int i, k;
+	int max[10];
+	long i, k, l;
 
+	l = n;
 	k = 0;
 	if (n < 0)
 	{
-		n = n * -1;
+		l = l * -1;
 		_putchar('-');
 	}
-	if (n ==0)
+	if (n == 0)
 		_putchar('0');
-	for (i = 0; n != 0 ; i++)
+	for (i = 0; l != 0 ; i++)
 	{
-		max[i] = (n % 10);
-		n = n / 10;
+		max[i] = (l % 10);
+		l = l / 10;
 		k = k + 1;
 	}
-	k = k -1;
+	k = k - 1;
 	while (k >= 0)
 	{
 		_putchar(max[k] + '0');
