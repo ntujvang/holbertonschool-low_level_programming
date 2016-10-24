9ec385db64b08d3db37d4fda00b183e27481ae95
nujivang <ntuj.vang@holbertonschool.com>
2016-10-24 20:59:47 +0000

    2

---
+++ 0x04-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - finding the length
+ * @s: string going in
+ * Return: int of how long string is
+ */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
