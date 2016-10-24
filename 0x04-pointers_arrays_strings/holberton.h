7f6780a1a08f4682844d1a7b9cdef46d735c6be7
nujivang <ntuj.vang@holbertonschool.com>
2016-10-24 23:13:11 +0000

    header

---
+++ 0x04-pointers_arrays_strings/holberton.h
@@ -0,0 +1,14 @@
+int _putchar(char c);
+/* stdout */
+void reset_to_98(int *n);
+/* ex 0 */
+void swap_int(int *a, int *b);
+/* ex 1 */
+int _strlen(char *s);
+/* ex 2 */
+void _puts(char *str);
+/* ex 3 */
+void print_rev(char *s);
+/* ex 4 */
+void rev_string(char *s);
+/* ex 5 */
