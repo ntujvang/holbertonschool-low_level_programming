4393a895aef77af5ebcb3ce8ce4fe03aa6726088
nujivang <ntuj.vang@holbertonschool.com>
2016-10-24 20:49:45 +0000

    1

---
+++ 0x04-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * swap_int - changing #
+ * @a: first #
+ * @b: second #
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
