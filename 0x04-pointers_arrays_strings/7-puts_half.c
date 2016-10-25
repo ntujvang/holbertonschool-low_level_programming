ad7e2a96597efa48293c3654389814fa35310832
nujivang <ntuj.vang@holbertonschool.com>
2016-10-25 18:21:40 +0000

    7

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -10,14 +10,14 @@ void puts_half(char *str)
 	int i, h, l, j, k;
 	int max[100];
 
-	i = strlen(str) -1;
+	i = strlen(str) - 1;
 	h = strlen(str) / 2;
 	j = i / 2;
 	if (i % 2 == 0)
 	{
 		for (k = j; k <= i; k++)
 		{
-			max[k] = str [k];
+			max[k] = str[k];
 			_putchar(max[k]);
 		}
 	}
