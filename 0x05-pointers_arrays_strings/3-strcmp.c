c27bf72bdb708310064683ef52749268b637913d
ntujvang <ntuj.vang@holbertonschool.com>
2016-10-27 20:19:24 +0000

    3

--- 0x05-pointers_arrays_strings/3-strcmp.c
+++ 0x05-pointers_arrays_strings/3-strcmp.c
@@ -7,22 +7,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, math;
+	int i;
 
 	i = 0;
-	if (s1[i] < s2[i])
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 	{
-		math = s1[i] - s2[i];
-		return (math);
+		i++;
 	}
-	else if (s1[i] > s2[i])
-	{
-		math = s1[i] - s2[i];
-		return (math);
-	}
-	else
-	{
-		math = s1[i] - s2[i];
-		return (math);
-	}
+	return (s1[i] - s2[i]);
 }
