d49b3af0f0ebed720723fc8e371664c18fac0090
ntujvang <ntuj.vang@holbertonschool.com>
2016-10-27 21:49:04 +0000

    6

--- 0x05-pointers_arrays_strings/6-cap_string.c
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -9,6 +9,12 @@ char *cap_string(char *l)
 	int i;
 
 	i = 0;
+	if (l[i] >= 'a' && l[i] <= 'z')
+	{
+		l[i] = 'A' + (l[i] - 'a');
+		i++;
+	}
+	i = 0;
 	while (l[i] != '\0')
 	{
 		if ((l[i] == ' ' || l[i] == '\t' || l[i] == '\n' || l[i] == ','
