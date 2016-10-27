2c6ae062315318fa6ec6facf0d7fcf187cfd5d40
ntujvang <ntuj.vang@holbertonschool.com>
2016-10-27 21:39:53 +0000

    6

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * *cap_string - caps to all beginners!
+ * @l: starting
+ * Return: some cap letter
+ */
+char *cap_string(char *l)
+{
+	int i;
+
+	i = 0;
+	while (l[i] != '\0')
+	{
+		if ((l[i] == ' ' || l[i] == '\t' || l[i] == '\n' || l[i] == ','
+		    || l[i] == ';' || l[i] == '.' || l[i] == '!' || l[i] == '?'
+		    || l[i] == '"' || l[i] == '(' || l[i] == ')' || l[i] == '{'
+		    || l[i] == '}') && (l[i + 1] >= 'a' && l[i + 1] <= 'z'))
+		{
+			l[i + 1] = 'A' + (l[i + 1] - 'a');
+		}
+		i++;
+	}
+	return (l);
+}
