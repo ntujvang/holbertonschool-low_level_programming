578c90fa73b3a61f0f8ac5e6f1bac6c3dcced603
ntujvang <ntuj.vang@holbertonschool.com>
2016-10-31 19:13:58 +0000

    4

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * *_strpbrk - matching letters
+ * @s: target ojective
+ * @accept: searching objective
+ * Return: string with the matching bytes at first appearance
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, n, k;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		n = 0;
+		k = 0;
+		while (accept[n] != '\0')
+		{
+			if (accept[n] == s[i])
+			{
+				k++;
+			}
+			n++;
+		}
+		if (k != 0)
+			return (s + i);
+		i++;
+	}
+	return (s + i);
+}
