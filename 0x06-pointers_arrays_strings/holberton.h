3e4789f08e321fa62a11b6d02d6dbeef0aca3eeb
ntujvang <ntuj.vang@holbertonschool.com>
2016-10-31 19:15:03 +0000

    header

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+/* stdout */
+char *_memset(char *s, char b, unsigned int n);
+/* ex 0 */
+char *_memcpy(char *dest, char *src, unsigned int n);
+/* ex 1 */
+char *_strchr(char *s, char c);
+/* ex 2 */
+unsigned int _strspn(char *s, char *accept);
+/* ex 3 */
+char *_strpbrk(char *s, char *accept);
+/* ex 4 */
