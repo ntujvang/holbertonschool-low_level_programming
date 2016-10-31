722c6bf9e2e321048e8258874b1098e7156eee29
ntujvang <ntuj.vang@holbertonschool.com>
2016-10-31 17:13:47 +0000

    2

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * *_strchr - finding where something appears first
+ * @s: target objective
+ * @c: target
+ * Return: everything after the target
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+		i++;
+	}
+	return (0);
+}
