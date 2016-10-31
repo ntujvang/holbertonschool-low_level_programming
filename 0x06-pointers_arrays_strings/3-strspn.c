d6ecc32df14eed73066511b17bcb64990d8ba59f
ntujvang <ntuj.vang@holbertonschool.com>
2016-10-31 17:59:15 +0000

    3

---
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ * int_strspn - count letters in first part only
+ * @s: target objective
+ * @accept: items searching for
+ * Return: number of bytes from s
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, n, k, y;
+
+	i = 0;
+	k = 0;
+	while (s[i] != '\0')
+	{
+		n = 0;
+		y = 0;
+		while (accept[n] != '\0')
+		{
+			if (accept[n] != s[i])
+				n++;
+			if (accept[n] == s[i])
+			{
+				k++;
+				n++;
+				y++;
+			}
+		}
+		if (y == 0)
+			return (k);
+		i++;
+	}
+	return (k);
+}
