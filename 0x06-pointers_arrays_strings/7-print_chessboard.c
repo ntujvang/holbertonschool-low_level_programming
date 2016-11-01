6950609079ba180f808eca47f5e4ca7c56f08d99
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-01 01:27:27 +0000

    7

---
+++ 0x06-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_chessboard - chessboard!!
+ * @a: first row
+ * @8: # of columns
+ * Return: chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, n;
+
+	i = 0;
+	while (i < 8)
+	{
+		n = 0;
+		while (n < 8)
+		{
+			_putchar (a[i][n]);
+			n++;
+		}
+		i++;
+		_putchar ('\n');
+	}
+}
