a68c677aa2d0a4857c91d13bdd4528dba1d817ea
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-02 23:21:17 +0000

    header

---
+++ 0x07-recursion/holberton.h
@@ -0,0 +1,24 @@
+int _putchar(char c);
+/* stdout */
+void _puts_recursion(char *s);
+/* ex 0 */
+void _print_rev_recursion(char *s);
+/* ex 1 */
+int _strlen_recursion(char *s);
+/* ex 2 */
+int factorial(int n);
+/* ex 3 */
+int _pow_recursion(int x, int y);
+/* ex 4 */
+int _sqrt_recursion(int n);
+/* ex 5 */
+int helpme(int n, int i);
+/* helper funtion for ex 5 */
+int is_prime_number(int n);
+/* ex 6 */
+int prime(int n, int i);
+/* helper function for ex 6 */
+int is_palindrome(char *s);
+/* ex 7 */
+int palindme(char *s, int i);
+/* helper for ex 7 */
