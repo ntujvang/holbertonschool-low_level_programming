1ed40498556cd74f27f08bb2660a753aabe08b70
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-06 16:37:46 +0000

    1

---
+++ 0x09-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - working area
+ * @argc: array count
+ * @argv: array list
+ * Return: 0 to end
+ */
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
