a704ebc403fcac1a0e3bab6317ea0c1f6988629c
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-06 18:45:04 +0000

    4

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -19,18 +19,18 @@ int main(int argc, char *argv[])
 	i = 1;
 	while (i < argc)
 	{
-		while (*argv[i] != '\0')
+		if (*argv[i] >= '0' && *argv[i] <= '9')
 		{
-			if (*argv[i] >= '0' && *argv[i] <= '9')
-			{
-				sum += atoi(argv[i]);
-			}
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-			argv[i]++;
+			sum += atoi(argv[i]);
+		}
+		else if (atoi(argv[i]) < 0)
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
 		}
 		i++;
 	}
