353f54cf300baaec2261cc37cc320ddd72834a3d
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-06 16:29:58 +0000

    0

---
+++ 0x09-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - working area
+ * @argc: # of arguments
+ * @argv: array of arguments
+ * Return: 0 to end
+ */
+int main(__attribute__ ((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
