581bbf2297ad24205187d543de2c47413416ccb0
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-06 18:11:54 +0000

    header

---
+++ 0x09-argc_argv/holberton.h
@@ -0,0 +1,8 @@
+int _putchar(char c);
+/* stdout */
+int main(__attribute__ ((unused)) int argc, char *argv[]);
+/* ex 0 */
+int main(int argc, __attribute__ ((unused)) char *argv[]);
+/* ex 1 */
+int main(int argc, char *argv[]);
+/* ex 2 */
