c918cc1beb0a3d664b3e5bbcbc1d29722ead5cf2
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-08 21:56:07 +0000

    4

---
+++ 0x0A-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - frees up a 2D grid
+ * @grid: double pointer to the grid
+ * @height: height of the grid
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	i = 0;
+	while (i < height)
+	{
+		free (grid[i]);
+		i++;
+	}
+	free (grid);
+}
