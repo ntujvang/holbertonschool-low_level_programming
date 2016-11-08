a94c67cce29eed410fc96d9dfb7d5231b49dab7e
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-08 20:22:48 +0000

    header

---
+++ 0x0A-malloc_free/holberton.h
@@ -0,0 +1,10 @@
+int _putchar(char c);
+/* stdout */
+char *create_array(unsigned int size, char c);
+/* ex 0 */
+char *_strdup(char *str);
+/* ex 1 */
+char *str_concat(char *s1, char *s2);
+/* ex 2 */
+int **alloc_grid(int width, int height);
+/* ex 3 */
