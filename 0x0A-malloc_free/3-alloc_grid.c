907069b01e638accc0b2a92fe772e4e0401c3e4e
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-08 20:37:52 +0000

    3

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * **alloc_grid - returns pointer to 2 newly created 2D int grid
+ * @width: width of the grid
+ * @height: heigth of the grid
+ * Return: pointers or NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int i;
+	int **s;
+
+	i = 0;
+	s = (int **) malloc(width * sizeof(int*));
+	while (i < width)
+	{
+		s[i] = (int *) malloc(height * sizeof(int));
+		i++;
+	}
+	return (s);
+}
