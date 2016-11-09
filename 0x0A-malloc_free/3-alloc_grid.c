42fb84027edb7c95ca82aaf75387c6d32ec347bd
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-09 00:53:16 +0000

    3

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -16,13 +16,21 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	s = (int **) malloc(height * sizeof(int *));
 	if (s == NULL)
+	{
+		free(s);
 		return (NULL);
+	}
 	i = 0;
 	while (i < height)
 	{
 		s[i] = (int *) malloc(width * sizeof(int *));
 		if (s[i] == '\0')
 		{
+			while (i >= 0)
+			{
+				free(s[i]);
+				i--;
+			}
 			free(s[i]);
 			return (NULL);
 		}
