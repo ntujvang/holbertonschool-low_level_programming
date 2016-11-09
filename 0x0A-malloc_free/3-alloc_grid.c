a75980acbac54dba5d825daaf97f44da99852038
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-09 01:09:00 +0000

    3

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -9,7 +9,7 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i;
+	int i, n;
 	int **s;
 
 	if (width <= 0 || height <= 0)
@@ -33,6 +33,17 @@ int **alloc_grid(int width, int height)
 			}
 			free(s[i]);
 			return (NULL);
+		}
+		i++;
+	}
+	i = 0;
+	while (i < height)
+	{
+		n = 0;
+		while (n < width)
+		{
+			s[i][n] = 0;
+			n++;
 		}
 		i++;
 	}
