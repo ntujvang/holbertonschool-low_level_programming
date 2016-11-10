755073377dbc80150aab5837964dc7ac046b08a1
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-10 22:36:14 +0000

    1

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -1,5 +1,6 @@
 #include "holberton.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
  * *string_nconcat - making a s1 string followed by the first n bytes of s2
  * @s1: first string
@@ -20,19 +21,20 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		;
 	for (c = 0; s2[c] != '\0'; c++)
 		;
-	c++;
-	if (n > c)
-	{
-		k = n - c;
-		c = n - k;
-	}
-	k = i + c;
+	k = 0;
+	printf("%d\n", i);
+	printf("%d\n", c);
+	if (n >= c)
+		k = i + c;
+	else
+		k = i + n;
+	printf("%d\n", k);
 	s = malloc(k * sizeof(*s));
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; s1[i] != '\0'; i++)
 		s[i] = s1[i];
-	for (c = 0; (c + i) <= k; c++, i++)
+	for (c = 0; i < k && s2[c] != '\0'; c++, i++)
 		s[i] = s2[c];
 	s[i] = '\0';
 	return (s);
