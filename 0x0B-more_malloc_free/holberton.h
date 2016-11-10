6de35a7e09a76780fb9e903a08d9977b9328cee8
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-10 21:16:33 +0000

    header

---
+++ 0x0B-more_malloc_free/holberton.h
@@ -0,0 +1,10 @@
+int _putchar(char c);
+/* stdout */
+void *malloc_checked(unsigned int b);
+/* ex 0 */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+/* ex 1 */
+void *_calloc(unsigned int nmemb, unsigned int size);
+/* ex 2 */
+int *array_range(int min, int max);
+/* ex 3 */
