19f2d3aa5bd2044abc99ab25636dec4d0039b610
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-14 23:35:59 +0000

    0

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog info
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* DOG */
