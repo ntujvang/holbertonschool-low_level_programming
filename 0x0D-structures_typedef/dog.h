07501ef62ff9f2434d72938aee363bd5042473b6
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-15 00:08:58 +0000

    with ex 4 mods

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -13,8 +13,11 @@ struct dog
 	char *owner;
 };
 typedef struct dog dog_t;
+/* ex 3 mods */
 void init_dog(struct dog *d, char *name, float age, char *owner);
 /* ex 1 */
 void print_dog(struct dog *d);
 /* ex 2 */
+dog_t *new_dog(char *name, float age, char *owner);
+/* ex 4 */
 #endif /* DOG */
