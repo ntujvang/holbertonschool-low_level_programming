0b530cf1c911af52fe6b4582dd39953db807ff2c
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-15 00:08:41 +0000

    4

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * *new_dog - a new dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ * Return: dog info
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *tmp;
+
+	tmp = malloc(sizeof(dog_t));
+	if (tmp == NULL)
+		return (NULL);
+	tmp->name = name;
+	tmp->age = age;
+	tmp-> owner = owner;
+	return (tmp);
+}
