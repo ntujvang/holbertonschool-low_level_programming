84ef8bb9c01b2f4528c42da6f19d00667d894a90
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-16 02:59:54 +0000

    4

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -9,13 +9,37 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *tmp;
+	dog_t *my_dog;
+	int i;
 
-	tmp = malloc(sizeof(dog_t));
-	if (tmp == NULL)
+	my_dog = malloc(sizeof(dog_t));
+	if (my_dog == NULL)
 		return (NULL);
-	tmp->name = name;
-	tmp->age = age;
-	tmp->owner = owner;
-	return (tmp);
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	if (i == 0 || name == NULL)
+		return (NULL);
+	my_dog->name = malloc(i);
+	if (my_dog->name == NULL)
+	{
+		free(my_dog);
+		return (NULL);
+	}
+	for (i = 0; name[i] != '\0'; i++)
+		my_dog->name[i] = name[i];
+	my_dog->age = age;
+	for (i = 0; owner[i] != '\0'; i++)
+		;
+	if (i == 0 || owner == NULL)
+		return (NULL);
+	my_dog->owner = malloc(i);
+	if (my_dog->owner == NULL)
+	{
+		free(my_dog);
+		free(my_dog->name);
+		return (NULL);
+	}
+	for (i = 0; owner[i] != '\0'; i++)
+		my_dog->owner[i] = owner[i];
+	return (my_dog);
 }
