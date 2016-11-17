ddfec31fb2ceae4921baf0eaac0eb0edffd6e06f
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-17 05:37:53 +0000

    header

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,3 +1,5 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 int _putchar(char c);
 /* stdout */
 void print_name(char *name, void (*f)(char *));
@@ -6,3 +8,4 @@ void array_iterator(int *array, size_t size, void (*action)(int));
 /* ex 1 */
 int int_index(int *array, int size, int (*cmp)(int));
 /* ex 2 */
+#endif
