3f96e72061b9cf9d285809ca74a2bd8425239133
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-20 23:04:11 +0000

    header

---
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -0,0 +1,18 @@
+#ifndef VAR_FUNC
+#define VAR_FUNC
+/* stdout */
+int _putchar(char c);
+/* ex 0 */
+int sum_them_all(const unsigned int n, ...);
+/* ex 1 */
+void print_numbers(const char *separator, const unsigned int n, ...);
+/* ex 2 */
+void print_strings(const char *separator, const unsigned int n, ...);
+/* ex 3 */
+void print_all(const char * const format, ...);
+typedef struct print_format
+{
+	char *p;
+	void (*f)();
+}print;
+#endif
