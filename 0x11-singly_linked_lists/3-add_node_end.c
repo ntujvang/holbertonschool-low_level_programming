a94e2a5b2a59e4d7b7fdb6798a3829d8f6e54f4a
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-29 05:36:25 +0000

    ex 3

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node_end - add a new node to the end
+ * @head: head of node
+ * @str: string to copy
+ * Return: new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *tmp;
+	int i;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	new->str = strdup(str);
+	new->len = i;
+	new->next = NULL;
+	tmp = *head;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = new;
+	tmp = tmp->next;
+	return(tmp);
+}
