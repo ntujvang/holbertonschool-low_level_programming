c3c27964d7a7ab4a4835e01ba3954de73097baba
ntujvang <ntuj.vang@holbertonschool.com>
2016-11-29 18:28:42 +0000

    prototype moved

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+#include "lists.h"
 /**
  * startUp - printing before main executes
  * Return: void
  */
-void startUp (void) __attribute__ ((constructor));
-void startUp (void)
+void startUp(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
