3d0963b8eda42168670ac48b8a65132f99c801fd
ntujvang <ntuj.vang@holbertonschool.com>
2016-12-01 21:55:22 +0000

    w/ ex 9

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -34,4 +34,6 @@ int pop_listint(listint_t **head);
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 /* ex 8 */
 int sum_listint(listint_t *head);
+/* ex 9 */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n);
 #endif
