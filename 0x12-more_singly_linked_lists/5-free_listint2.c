eb63abc33c3c6af6e4b0a1bac8ce89286b5d6c61
ntujvang <ntuj.vang@holbertonschool.com>
2016-12-01 19:42:16 +0000

    5

---
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - free a list
+ * @head: pointer to the list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp, *hold;
+
+	hold = *head;
+	while (hold != NULL)
+	{
+		tmp = hold;
+		hold = hold->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
