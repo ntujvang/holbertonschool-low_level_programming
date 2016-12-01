7fecbb59268b648c681fa67ea48ad952391159bb
ntujvang <ntuj.vang@holbertonschool.com>
2016-12-01 20:37:06 +0000

    seriously?!

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -10,9 +10,8 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp, *hold;
 
-	if (head != NULL)
-		hold = *head;
-	while (hold != NULL)
+	hold = *head;
+	while (hold != NULL && *head != NULL)
 	{
 		tmp = hold;
 		hold = hold->next;
