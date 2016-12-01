e5f5ec6582a99bcba917ac5005f58b8238cbe203
ntujvang <ntuj.vang@holbertonschool.com>
2016-12-01 19:44:45 +0000

    head fix

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -11,7 +11,7 @@ void free_listint2(listint_t **head)
 	listint_t *tmp, *hold;
 
 	hold = *head;
-	while (hold != NULL)
+	while (hold != NULL && head != NULL)
 	{
 		tmp = hold;
 		hold = hold->next;
