bf225fa77e83d966e30342510d8908093eec0272
ntujvang <ntuj.vang@holbertonschool.com>
2016-12-02 05:00:06 +0000

    9

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,6 +15,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 
 	if (*head == NULL && index != 0)
 		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	if (index == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	i = 0;
 	tmp = *head;
 	while (i < (index - 1) && tmp != NULL)
@@ -23,18 +33,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n)
 		tmp = tmp->next;
 	}
 	if ((index - 1) != i)
+	{
+		free(new);
 		return (NULL);
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = n;
+	}
 	new->next = tmp->next;
 	tmp->next = new;
-	if (index == 0)
-	{
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
 	return (new);
 }
