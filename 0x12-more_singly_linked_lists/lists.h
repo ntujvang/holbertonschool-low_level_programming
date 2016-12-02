6db0864b5ec5f9098f50757c72f3c1f14321af4f
ntujvang <ntuj.vang@holbertonschool.com>
2016-12-02 20:51:59 +0000

    proto error

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -35,7 +35,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 /* ex 8 */
 int sum_listint(listint_t *head);
 /* ex 9 */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int index, int n);
+listint_t *insert_nodeint_at_index
+(listint_t **head, unsigned int index, int n);
 /* ex 10 */
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 /* ex 11 */
