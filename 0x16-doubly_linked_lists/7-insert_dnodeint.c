ccbc845ef891af078671efaae4fde8a409179c86
ntujvang <ntuj.vang@holbertonschool.com>
2017-01-10 01:42:08 +0000

    ?

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *temp, *new, *tail;
 	unsigned int i;
 
-	if (*h == NULL)
+	if (*h == NULL && idx != 0)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
