16db17b735386b90dcf51e1084766931966d24fe
ntujvang <ntuj.vang@holbertonschool.com>
2017-01-10 01:03:19 +0000

    betty doc issue

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -2,7 +2,7 @@
 /**
  * add_dnodeint - add node to beginning of list
  * @head: pointer to the head
- * n: value of the node
+ * @n: value of the node
  * Return: address of new element or NULL
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
