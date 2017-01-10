c3475d1f39fbe3138219108e9b0bf9d86dac0f93
ntujvang <ntuj.vang@holbertonschool.com>
2017-01-10 01:29:40 +0000

    ?

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,5 +41,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new->next = tail->next;
 	new->prev = tail->prev;
 	tail->next = new;
+	new->prev = tail;
 	return (new);
 }
