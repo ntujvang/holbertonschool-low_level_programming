f41312b834adc8a42eb4f2a66ebf04523482c098
ntujvang <96@holbertonschool.com>
2017-03-28 00:48:00 +0000

    added func null check and wrong return for 9

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -9,7 +9,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	int leftheight, rightheight;
 
 	if (tree == NULL)
-		return (-1);
+		return (0);
 	leftheight = binary_tree_height(tree->left);
 	rightheight = binary_tree_height(tree->right);
 	if (leftheight > rightheight)
