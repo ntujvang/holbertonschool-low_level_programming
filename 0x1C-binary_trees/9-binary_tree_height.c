7cfd296b4e2b786f465cc04c626cd02e93dd9039
ntujvang <96@holbertonschool.com>
2017-03-28 00:52:06 +0000

    new check

--- 0x1C-binary_trees/9-binary_tree_height.c
+++ 0x1C-binary_trees/9-binary_tree_height.c
@@ -10,14 +10,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (-1);
-	if (tree->left == NULL && tree->right == NULL)
+	if (tree->left == NULL && tree->right == NULL && tree != NULL)
 		return (0);
 	leftheight = binary_tree_height(tree->left);
 	rightheight = binary_tree_height(tree->right);
 	if (leftheight > rightheight)
-	{
 		return (leftheight + 1);
-	}
 	return (rightheight + 1);
 
 }
